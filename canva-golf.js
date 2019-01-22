e1d5a5b0d3a31023b8cb78fc2d1c2a8169c5b99b
drzhbe <drzhbe@gmail.com>
2019-01-23 09:08:42 +1100

    1096

--- canva-golf.js
+++ canva-golf.js
@@ -1,21 +1,21 @@
-let c = document.querySelector('canvas'),
-    w = window,
-    u = w.z, // undefined
+let d = document,
+    c = d.querySelector('canvas'),
+    w = this,
+    u, // undefined
     dpr = devicePixelRatio || 1,
-    ww = w.innerWidth,
-    wh = w.innerHeight,
-    l = document.addEventListener,
-    rAQ = requestAnimationFrame,
+    W = w.innerWidth,
+    H = w.innerHeight,
+    l = d.addEventListener,
     ctx = c.getContext('2d'),
-    M = Math,
     FONT_SIZE = 20,
+    TEXT = 'Golf Wang',
     dl = e => {
       e.target.href = c.toDataURL()
       e.target.download = 'canva-golf.png'
     },
     types = {i:0,t:1,r:2}, // types: image, text, rectangle
     elements = [],
-    texts = [],
+    texts = [], // separate arrays for elements and texts just to maintain the Z index: text is always on top of images
     dragging,
     E = (t,c,x,y,w,h) => ({t,c,x,y,w,h}), // type, content, x, y, width, height
     addImage = i => {
@@ -23,35 +23,23 @@ let c = document.querySelector('canvas'),
           ih = i.height,
           dw = iw,
           dh = ih
-      if (iw > ih) {   // scale down images so they exactly match
-        if (iw > ww) { // one of the dimensions of the screen
-          dw = ww
-          dh = ih * ww / iw
-        }
-      } else {
-        if (ih > wh) {
-          dh = wh
-          dw = iw * wh / ih
-        }
-      }
-
-      // iw > ih
-      //     ? iw > ww && (dw = ww, dh = ih * ww / iw)
-      //     : ih > wh && (dh = wh, dw = iw * wh / ih);
-
+      // scale down images so they exactly match
+      // one of the dimensions of the screen
+      iw > ih
+          ? iw > W && (dw = W, dh = ih * W / iw)
+          : ih > H && (dh = H, dw = iw * H / ih);
       elements.push(E(types.i, i, 0, 0, dw, dh))
     },
-    addText = t => texts.push(E(types.t, t, ww / 2 - ctx.measureText(t).width / 2, wh / 2 - FONT_SIZE, w, FONT_SIZE)),
+    addText = t => texts.push(E(types.t, t, W / 2 - ctx.measureText(t).width / 2, H / 2 - FONT_SIZE, w, FONT_SIZE)),
     drop = e => {
       e.preventDefault()
       const files = [...e.dataTransfer.items].map(i => i.getAsFile())
-      const images = files.map(f => {
+      files.forEach(f => {
         const i = new Image()
         i.onload = () => addImage(i)
         i.src = URL.createObjectURL(f)
       })
     },
-    dragover = e => e.preventDefault(),
     pointInElement  = (x,y,e)  => x > e.x && x < e.x + e.w && y > e.y && y < e.y + e.h,
     pointedElement  = (x,y)    => pointedElement_(x, y, texts, texts.length) || pointedElement_(x, y, elements, elements.length),
     // first check last added els (with largest Z index)
@@ -65,19 +53,19 @@ let c = document.querySelector('canvas'),
       e => ctx.fillText(e.c, e.x, e.y + FONT_SIZE),
     ],
     render = _ => {
-      ctx.clearRect(0,0,ww,wh)
+      ctx.clearRect(0,0,W,H)
       elements.concat(texts).forEach(e => elementRenderers[e.t](e))
-      rAQ(render)
+      requestAnimationFrame(render)
     }
-c.width = ww * dpr
-c.height = wh * dpr
+c.width = W * dpr
+c.height = H * dpr
 ctx.scale(dpr, dpr)
 ctx.font = FONT_SIZE + 'pt Serif'
 l('mousedown', e => {
   element = pointedElement(e.x, e.y)
-  dragging = element && { e: element, dx: e.x - element.x, dy: e.y - element.y }
-  l('mouseup',e=>dragging=u)
+  dragging = element && { e: element, dx: e.x - element.x, dy: e.y - element.y } // save the offset from drag start mouse position to the element's top left
 })
+l('mouseup',e=>dragging=u)
 l('mousemove',e=>dragging&&dragElement(dragging,e.x,e.y))
-l('keydown',e=>e.key=='t'&&addText('Golf Wang'))
-rAQ(render)
+l('keydown',e=>e.key=='t'&&addText(TEXT))
+render()
